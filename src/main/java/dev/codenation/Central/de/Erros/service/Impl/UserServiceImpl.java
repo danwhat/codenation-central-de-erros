f29c012f96971c87f9df84c552ac81d2004e01d0
Danwhat <daanwhat@gmail.com>
2021-04-19 17:56:53 -0300

    Oauth2 implemented

--- src/main/java/dev/codenation/Central/de/Erros/service/Impl/UserServiceImpl.java
+++ src/main/java/dev/codenation/Central/de/Erros/service/Impl/UserServiceImpl.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserServiceInterface {
-
     private final UserRepository userRepository;
 
     public UserServiceImpl(UserRepository userRepository) {
